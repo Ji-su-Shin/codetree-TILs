c00d937ba73f726100f5e4167cdaa7affbccae4e
Shin-jisu <81508062+Ji-su-Shin@users.noreply.github.com>
2024-10-02 21:22:42 +0900

    Title: 길이 단위 변환하기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241002/길이 단위 변환하기/change-length-unit.py
@@ -0,0 +1,7 @@
+num_ft = 9.2
+num_mi = 1.3
+convert_ft_to_cm = num_ft * 30.48
+convert_mi_to_cm = num_mi * 160934
+
+print(f"{num_ft}ft = {convert_ft_to_cm:.1f}cm")
+print(f"{num_mi}mi = {convert_mi_to_cm:.1f}cm")
